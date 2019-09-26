bc845d6e344049a1c489ca8c58853126ae001e01
ishizzle8D <ishizzleclem@gmail.com>
2019-09-26 13:59:38 -0400

    personal site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-// MDB Lightbox Init
-$(function () {
-    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+
+
+
